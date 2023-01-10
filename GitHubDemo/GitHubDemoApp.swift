3e0cdb9ed73cacd6f3323611fbe2fa214b7d3076
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-01-10 08:32:05 -0600

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Matthew RIley on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
